f3521d1f1c9d681f1c87aeb6770065d6c26bb2c9
Alejandra <Alejandra@DESKTOP-HQKA5R3>
2026-03-01 13:12:36 -0600

    uso de for anidado y fin del programa

--- MendezAlejandra_foranidado7/Program.cs
+++ MendezAlejandra_foranidado7/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Mostrar las combinaciones posibles de números del 1 al 3");
+
+for (int i = 1; i <= 3; i++)
+{
+    for (int j = 1; j <= 3; j++)
+    {
+        Console.WriteLine(i + "-" + j);
+    }
+}
